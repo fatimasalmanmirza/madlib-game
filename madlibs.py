0f06206f1459b5a54786cfd9edfe24a5d5a28139
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-23 17:37:47 -0800

    added place

--- madlibs.py
+++ madlibs.py
@@ -60,7 +60,8 @@ def show_madlib():
     color = request.args.get("color")
     noun = request.args.get("noun")
     adjective = request.args.get("adjective")
-    return render_template("madlib.html",person=person, color=color, noun=noun, adjective=adjective)            
+    place = request.args.get("place")
+    return render_template("madlib.html",person=person, color=color, noun=noun, adjective=adjective, place=place)            
 
 
 
